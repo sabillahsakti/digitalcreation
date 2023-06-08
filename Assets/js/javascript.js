4a46ade0bbb11bb3ff24c3279e0d9471ff434abc
sabillahsakti <sabilajja63@gmail.com>
2023-06-08 07:45:48 +0700

    bismillah selesai

--- Assets/js/javascript.js
+++ Assets/js/javascript.js
@@ -2,6 +2,17 @@ var sliderIndex = 0;
 var sliderTrack = document.querySelector('.slider-track');
 var slides = document.querySelectorAll('.slider-track img');
 var slideWidth = slides[0].clientWidth;
+var autoSlideInterval;
+
+function startAutoSlide() {
+    autoSlideInterval = setInterval(function () {
+        changeSlide(1);
+    }, 3000); // Ganti slide setiap 3 detik
+}
+
+function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+}
 
 function changeSlide(n) {
     sliderIndex += n;
@@ -14,3 +25,5 @@ function changeSlide(n) {
 
     sliderTrack.style.transform = `translateX(-${sliderIndex * slideWidth}px)`;
 }
+
+startAutoSlide();
